f27d526e128a93eed28a74b6ef2385ca09005d98
Matt Klein <mklein@bitdrift.io>
2024-10-23 15:20:25 -0600

    fix
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- ci/check_license.sh
+++ ci/check_license.sh
@@ -6,6 +6,6 @@ python3 ./ci/license_header.py
 
 # Check if git repository is dirty
 if [[ -n $(git status --porcelain) ]]; then
-  echo "Error: Git repository is dirty. Run ci/license_headers.py to update license headers."
+  echo "Error: Git repository is dirty. Run ci/check_license.sh to update license headers."
   exit 1
 fi
--- pulse-metrics/src/pipeline/inflow/wire/pre_buffer.rs
+++ pulse-metrics/src/pipeline/inflow/wire/pre_buffer.rs
@@ -1,3 +1,10 @@
+// pulse - bitdrift's observability proxy
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
 use crate::protos::metric::{
   default_timestamp,
   CounterType,
