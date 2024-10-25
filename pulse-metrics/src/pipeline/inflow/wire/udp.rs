1acf5d776f0a6b8a585aac7e2a8c995d1ae67be1
Matt Klein <mattklein123@gmail.com>
2024-10-25 08:22:04 -0600

    add UDP pre-buffering (#14)
    This is similar to my previous commit, but for UDP. If configured, it creates
    UDP "sessions" which have a pre-buffer config as well as a session idle
    timeout.
    
    Fixes BIT-3990
    
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- pulse-metrics/src/pipeline/inflow/wire/udp.rs
+++ pulse-metrics/src/pipeline/inflow/wire/udp.rs
@@ -5,24 +5,252 @@
 // LICENSE file or at:
 // https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
 
-use super::util::{parse_lines, process_buffer_newlines};
+use super::util::{parse_lines, process_buffer_newlines, PreBufferWrapper};
 use crate::pipeline::inflow::wire::util::bind_k8s_metadata;
 use crate::pipeline::inflow::PipelineInflow;
 use crate::pipeline::{ComponentShutdown, InflowFactoryContext, PipelineDispatch};
 use crate::protos::metric::DownstreamId;
+use ahash::AHashMap;
 use async_trait::async_trait;
 use bd_server_stats::stats::Scope;
 use bd_shutdown::ComponentShutdownTriggerHandle;
-use bytes::BytesMut;
+use bd_time::ProtoDurationExt;
+use bytes::{Bytes, BytesMut};
 use log::{info, warn};
 use parking_lot::Mutex;
 use prometheus::IntCounter;
 use pulse_common::k8s::pods_info::OwnedPodsInfoSingleton;
 use pulse_protobuf::protos::pulse::config::inflow::v1::wire::UdpServerConfig;
+use std::collections::hash_map::Entry;
+use std::mem::MaybeUninit;
+use std::net::IpAddr;
+use std::ptr;
 use std::sync::Arc;
 use std::time::Instant;
+use time::Duration;
 use tokio::net::UdpSocket;
-use tokio::select;
+use tokio::sync::mpsc;
+use tokio::time::{sleep, Instant as TokioInstant};
+use tokio::{pin, select};
+
+struct Shared {
+  stats: UdpServerStats,
+  config: UdpServerConfig,
+  dispatcher: Arc<dyn PipelineDispatch>,
+  k8s_pods_info: Option<OwnedPodsInfoSingleton>,
+  shutdown: ComponentShutdownTriggerHandle,
+}
+
+//
+// SessionManager
+//
+
+#[async_trait]
+trait SessionManager: Send + Sync {
+  async fn recv_lines(&mut self, lines: Vec<Bytes>, peer_ip: IpAddr);
+}
+
+//
+// DefaultSessionManager
+//
+
+struct DefaultSessionManager {
+  shared: Arc<Shared>,
+}
+
+#[async_trait]
+impl SessionManager for DefaultSessionManager {
+  async fn recv_lines(&mut self, lines: Vec<Bytes>, peer_ip: IpAddr) {
+    let received_at = Instant::now();
+    let downstream_id = DownstreamId::IpAddress(peer_ip);
+    let mut parsed_lines = parse_lines(
+      lines,
+      &self.shared.config.protocol,
+      received_at,
+      &downstream_id,
+      &self.shared.stats.unparsable,
+    );
+    bind_k8s_metadata(
+      &mut parsed_lines,
+      &downstream_id,
+      &self.shared.stats.no_k8s_pod_metadata,
+      self.shared.k8s_pods_info.as_ref(),
+    );
+    self.shared.dispatcher.send(parsed_lines).await;
+  }
+}
+
+//
+// PreBufferSessionManager
+//
+
+struct PreBufferSessionManager {
+  shared: Arc<Shared>,
+  sessions: Arc<Mutex<AHashMap<IpAddr, mpsc::Sender<Vec<Bytes>>>>>,
+  pre_buffer_window: Duration,
+  session_idle_timeout: Duration,
+}
+
+#[async_trait]
+impl SessionManager for PreBufferSessionManager {
+  async fn recv_lines(&mut self, lines: Vec<Bytes>, peer_ip: IpAddr) {
+    let session = self
+      .sessions
+      .lock()
+      .entry(peer_ip)
+      .or_insert_with(|| {
+        let (tx, rx) = mpsc::channel(1);
+        log::debug!("creating new session for {}", peer_ip);
+        tokio::spawn(
+          PreBufferSession::new(
+            self.shared.clone(),
+            self.sessions.clone(),
+            peer_ip,
+            self.pre_buffer_window,
+          )
+          .run(
+            rx,
+            self.shared.shutdown.make_shutdown(),
+            self.session_idle_timeout,
+          ),
+        );
+        tx
+      })
+      .clone();
+    // We guarantee that the receiver is never removed if the strong count is greater than 1. Thus
+    // this should never fail.
+    session.send(lines).await.unwrap();
+  }
+}
+
+//
+// PreBufferSession
+//
+
+struct PreBufferSession {
+  shared: Arc<Shared>,
+  sessions: Arc<Mutex<AHashMap<IpAddr, mpsc::Sender<Vec<Bytes>>>>>,
+  peer_ip: IpAddr,
+  pre_buffer: Option<PreBufferWrapper>,
+}
+
+impl PreBufferSession {
+  fn new(
+    shared: Arc<Shared>,
+    sessions: Arc<Mutex<AHashMap<IpAddr, mpsc::Sender<Vec<Bytes>>>>>,
+    peer_ip: IpAddr,
+    pre_buffer_window: Duration,
+  ) -> Self {
+    Self {
+      shared,
+      sessions,
+      peer_ip,
+      pre_buffer: Some(PreBufferWrapper::new(pre_buffer_window)),
+    }
+  }
+
+  async fn run(
+    mut self,
+    mut rx: mpsc::Receiver<Vec<Bytes>>,
+    mut shutdown: ComponentShutdown,
+    session_idle_timeout: Duration,
+  ) {
+    let idle_timeout = sleep(session_idle_timeout.unsigned_abs());
+    pin!(idle_timeout);
+    let shutdown_future = shutdown.cancelled();
+    pin!(shutdown_future);
+
+    loop {
+      idle_timeout
+        .as_mut()
+        .reset(TokioInstant::now() + session_idle_timeout.unsigned_abs());
+
+      select! {
+        lines = rx.recv() => {
+          // We control when things leave the map so there should always be a sender.
+          self.process(lines.unwrap()).await;
+        },
+        () = async {
+          self.pre_buffer.as_mut().unwrap().sleep.as_mut().await;
+        }, if self.pre_buffer.is_some() => {
+          self.flush_pre_buffer().await;
+          continue;
+        },
+        () = &mut idle_timeout => {
+          if self.on_idle_timeout() {
+            log::debug!("session idle timeout for {}", self.peer_ip);
+            break;
+          }
+          continue;
+        },
+        () = &mut shutdown_future => {
+          break;
+        }
+      }
+    }
+
+    // If we lost the connection prior to the pre-buffer timeout, try to flush what we have.
+    if self.pre_buffer.is_some() {
+      self.flush_pre_buffer().await;
+    }
+  }
+
+  fn on_idle_timeout(&self) -> bool {
+    let mut sessions = self.sessions.lock();
+    if let Entry::Occupied(o) = sessions.entry(self.peer_ip) {
+      // If the strong count is 1 under lock, we have the only reference, and thus it is safe to
+      // remove from the map. Any new packets will create a new session with a new channel. If
+      // however the strong count is > 1 this means that there was a race and a packet is in the
+      // process of being sent to this channel. Keep the channel open and loop around again.
+      if o.get().strong_count() == 1 {
+        o.remove();
+        return true;
+      }
+      return false;
+    }
+    true
+  }
+
+  async fn process(&mut self, lines: Vec<Bytes>) {
+    let downstream_id = DownstreamId::IpAddress(self.peer_ip);
+    let mut parsed_lines = parse_lines(
+      lines,
+      &self.shared.config.protocol,
+      Instant::now(),
+      &downstream_id,
+      &self.shared.stats.unparsable,
+    );
+    if let Some(pre_buffer) = &mut self.pre_buffer {
+      pre_buffer.buffer.buffer(parsed_lines);
+    } else {
+      bind_k8s_metadata(
+        &mut parsed_lines,
+        &downstream_id,
+        &self.shared.stats.no_k8s_pod_metadata,
+        self.shared.k8s_pods_info.as_ref(),
+      );
+      self.shared.dispatcher.send(parsed_lines).await;
+    }
+  }
+
+  async fn flush_pre_buffer(&mut self) {
+    let downstream_id = DownstreamId::IpAddress(self.peer_ip);
+    let pre_buffer = self.pre_buffer.take().unwrap();
+    let mut metrics = pre_buffer.buffer.flush(&downstream_id);
+    log::debug!("flushed prebuffer with {} metrics", metrics.len());
+    bind_k8s_metadata(
+      &mut metrics,
+      &downstream_id,
+      &self.shared.stats.no_k8s_pod_metadata,
+      self.shared.k8s_pods_info.as_ref(),
+    );
+    self.shared.dispatcher.send(metrics).await;
+  }
+}
+
+//
+// UdpServerStats
+//
 
 #[derive(Clone, Debug)]
 struct UdpServerStats {
@@ -41,13 +269,13 @@ impl UdpServerStats {
   }
 }
 
+//
+// UdpInflow
+//
+
 pub struct UdpInflow {
-  scope: Scope,
-  config: UdpServerConfig,
-  dispatcher: Arc<dyn PipelineDispatch>,
-  shutdown_trigger_handle: ComponentShutdownTriggerHandle,
+  shared: Arc<Shared>,
   socket: Mutex<Option<UdpSocket>>,
-  k8s_pods_info: Option<OwnedPodsInfoSingleton>,
 }
 
 impl UdpInflow {
@@ -58,18 +286,23 @@ impl UdpInflow {
       None
     };
 
+    let stats = UdpServerStats::new(&context.scope);
+    let shared = Arc::new(Shared {
+      stats,
+      config,
+      dispatcher: context.dispatcher,
+      k8s_pods_info,
+      shutdown: context.shutdown_trigger_handle,
+    });
+
     let socket = context
       .bind_resolver
-      .resolve_udp(config.bind.as_str())
+      .resolve_udp(shared.config.bind.as_str())
       .await?;
     info!("udp server running on {}", socket.local_addr().unwrap());
     Ok(Self {
-      scope: context.scope,
-      config,
-      dispatcher: context.dispatcher,
-      shutdown_trigger_handle: context.shutdown_trigger_handle,
+      shared,
       socket: Mutex::new(Some(socket)),
-      k8s_pods_info,
     })
   }
 }
@@ -77,55 +310,54 @@ impl UdpInflow {
 #[async_trait]
 impl PipelineInflow for UdpInflow {
   async fn start(self: Arc<Self>) {
-    let stats = UdpServerStats::new(&self.scope);
+    let session_manager = self.shared.config.pre_buffer.as_ref().map_or_else(
+      || {
+        Box::new(DefaultSessionManager {
+          shared: self.shared.clone(),
+        }) as Box<dyn SessionManager>
+      },
+      |pre_buffer| {
+        Box::new(PreBufferSessionManager {
+          shared: self.shared.clone(),
+          sessions: Arc::default(),
+          pre_buffer_window: pre_buffer.pre_buffer_window.to_time_duration(),
+          session_idle_timeout: pre_buffer.session_idle_timeout.to_time_duration(),
+        }) as Box<dyn SessionManager>
+      },
+    );
 
     tokio::spawn(udp_reader(
-      stats,
-      self.config.clone(),
+      self.shared.clone(),
       self.socket.lock().take().unwrap(),
-      self.dispatcher.clone(),
-      self.k8s_pods_info.clone(),
-      self.shutdown_trigger_handle.make_shutdown(),
+      session_manager,
+      self.shared.shutdown.make_shutdown(),
     ));
   }
 }
 
 async fn udp_reader(
-  stats: UdpServerStats,
-  config: UdpServerConfig,
+  shared: Arc<Shared>,
   socket: UdpSocket,
-  dispatcher: Arc<dyn PipelineDispatch>,
-  k8s_pods_info: Option<OwnedPodsInfoSingleton>,
+  mut session_manager: Box<dyn SessionManager>,
   mut shutdown: ComponentShutdown,
 ) {
   let buffer_size = 65507;
   let mut buf = BytesMut::with_capacity(buffer_size);
   loop {
-    buf.resize(buffer_size, 0);
+    buf.reserve(buffer_size);
     select! {
-      res = socket.recv_from(buf.as_mut()) => {
-        let received_at = Instant::now();
+      res = socket.recv_from(
+        unsafe {
+          &mut *(ptr::from_mut::<[MaybeUninit<u8>]>(buf.spare_capacity_mut()) as *mut [u8])
+        }) => {
         match res {
           Ok((bytes, peer_addr)) => {
             log::trace!("udp recv from={peer_addr} len={bytes}");
-            stats.incoming_bytes.inc_by(bytes.try_into().unwrap());
-            buf.truncate(bytes);
+            shared.stats.incoming_bytes.inc_by(bytes.try_into().unwrap());
+            unsafe { buf.set_len(bytes); }
             let lines = process_buffer_newlines(&mut buf, false);
-            let downstream_id = DownstreamId::IpAddress(peer_addr.ip());
-            let mut parsed_lines = parse_lines(
-              lines,
-              &config.protocol,
-              received_at,
-              &downstream_id,
-              &stats.unparsable,
-            );
-            bind_k8s_metadata(
-              &mut parsed_lines,
-              &downstream_id,
-              &stats.no_k8s_pod_metadata,
-              k8s_pods_info.as_ref(),
-            );
-            dispatcher.send(parsed_lines).await;
+            debug_assert!(buf.is_empty());
+            session_manager.recv_lines(lines, peer_addr.ip()).await;
           },
           Err(e) => warn!("udp receiver error: {}", e),
         }
@@ -135,6 +367,6 @@ async fn udp_reader(
       }
     }
   }
-  info!("terminated udp server running on {}", config.bind);
+  info!("terminated udp server running on {}", shared.config.bind);
   drop(shutdown);
 }
