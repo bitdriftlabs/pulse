ecd579a31ec210c93404e0faff370f552452aaee
Matt Klein <mklein@bitdrift.io>
2025-08-27 10:02:06 -0600

    drop: fix over eager matching for exact matches
    Universally using regex was causing a prefix match.
    
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- pulse-metrics/src/pipeline/processor/drop/mod_test.rs
+++ pulse-metrics/src/pipeline/processor/drop/mod_test.rs
@@ -28,6 +28,126 @@ use prometheus::labels;
 use pulse_protobuf::protos::pulse::config::processor::v1::drop;
 use std::sync::Arc;
 
+fn make_exact_match(name: &str) -> DropCondition {
+  DropCondition {
+    condition_type: Some(Condition_type::MetricName(StringMatch {
+      string_match_type: Some(String_match_type::Exact(name.to_string().into())),
+      ..Default::default()
+    })),
+    ..Default::default()
+  }
+}
+
+fn make_regex_match(name: &str) -> DropCondition {
+  DropCondition {
+    condition_type: Some(Condition_type::MetricName(StringMatch {
+      string_match_type: Some(String_match_type::Regex(name.to_string().into())),
+      ..Default::default()
+    })),
+    ..Default::default()
+  }
+}
+
+fn make_not_match(condition: DropCondition) -> DropCondition {
+  DropCondition {
+    condition_type: Some(Condition_type::NotMatch(Box::new(condition))),
+    ..Default::default()
+  }
+}
+
+fn make_tag_match(name: &str) -> DropCondition {
+  DropCondition {
+    condition_type: Some(Condition_type::TagMatch(TagMatch {
+      tag_name: name.to_string().into(),
+      ..Default::default()
+    })),
+    ..Default::default()
+  }
+}
+
+fn make_tag_value_exact_match(tag_name: &str, tag_value: &str) -> DropCondition {
+  DropCondition {
+    condition_type: Some(Condition_type::TagMatch(TagMatch {
+      tag_name: tag_name.to_string().into(),
+      tag_value: Some(StringMatch {
+        string_match_type: Some(String_match_type::Exact(tag_value.to_string().into())),
+        ..Default::default()
+      })
+      .into(),
+      ..Default::default()
+    })),
+    ..Default::default()
+  }
+}
+
+fn make_tag_value_regex_match(tag_name: &str, tag_value: &str) -> DropCondition {
+  DropCondition {
+    condition_type: Some(Condition_type::TagMatch(TagMatch {
+      tag_name: tag_name.to_string().into(),
+      tag_value: Some(StringMatch {
+        string_match_type: Some(String_match_type::Regex(tag_value.to_string().into())),
+        ..Default::default()
+      })
+      .into(),
+      ..Default::default()
+    })),
+    ..Default::default()
+  }
+}
+
+fn make_and_match(conditions: Vec<DropCondition>) -> DropCondition {
+  DropCondition {
+    condition_type: Some(Condition_type::AndMatch(AndMatch {
+      conditions,
+      ..Default::default()
+    })),
+    ..Default::default()
+  }
+}
+
+#[tokio::test]
+async fn regex_vs_exact() {
+  let (mut helper, context) = processor_factory_context_for_test();
+  let processor = Arc::new(
+    DropProcessor::new(
+      DropProcessorConfig {
+        config_source: Some(Config_source::Inline(DropConfig {
+          rules: vec![DropRule {
+            name: "rule1".into(),
+            mode: DropMode::ENABLED.into(),
+            conditions: vec![make_exact_match("exact_name")],
+            ..Default::default()
+          }],
+          ..Default::default()
+        })),
+        ..Default::default()
+      },
+      context,
+    )
+    .await
+    .unwrap(),
+  );
+
+  make_mut(&mut helper.dispatcher)
+    .expect_send()
+    .times(1)
+    .returning(|metrics| {
+      assert_eq!(metrics, vec![make_metric("exact_name_with_more", &[], 0),]);
+    });
+  processor
+    .clone()
+    .recv_samples(vec![
+      make_metric("exact_name", &[], 0),
+      make_metric("exact_name_with_more", &[], 0),
+    ])
+    .await;
+  helper.stats_helper.assert_counter_eq(
+    1,
+    "processor:dropped",
+    &labels! { "rule_name" => "rule1", "mode" => "enabled" },
+  );
+}
+
 #[tokio::test]
 async fn not() {
   let (mut helper, context) = processor_factory_context_for_test();
@@ -38,16 +158,7 @@ async fn not() {
           rules: vec![DropRule {
             name: "rule1".into(),
             mode: DropMode::ENABLED.into(),
-            conditions: vec![DropCondition {
-              condition_type: Some(Condition_type::NotMatch(Box::new(DropCondition {
-                condition_type: Some(Condition_type::MetricName(StringMatch {
-                  string_match_type: Some(String_match_type::Exact("exact_name".into())),
-                  ..Default::default()
-                })),
-                ..Default::default()
-              }))),
-              ..Default::default()
-            }],
+            conditions: vec![make_not_match(make_exact_match("exact_name"))],
             ..Default::default()
           }],
           ..Default::default()
@@ -92,85 +203,31 @@ async fn all() {
               name: "rule1".into(),
               mode: DropMode::ENABLED.into(),
               conditions: vec![
-                DropCondition {
-                  condition_type: Some(Condition_type::MetricName(StringMatch {
-                    string_match_type: Some(String_match_type::Exact("exact_name".into())),
-                    ..Default::default()
-                  })),
-                  ..Default::default()
-                },
-                DropCondition {
-                  condition_type: Some(Condition_type::MetricName(StringMatch {
-                    string_match_type: Some(String_match_type::Regex("regex_name.*".into())),
-                    ..Default::default()
-                  })),
-                  ..Default::default()
-                },
-                DropCondition {
-                  condition_type: Some(Condition_type::TagMatch(TagMatch {
-                    tag_name: "tag_present".into(),
-                    ..Default::default()
-                  })),
-                  ..Default::default()
-                },
-                DropCondition {
-                  condition_type: Some(Condition_type::TagMatch(TagMatch {
-                    tag_name: "tag_exact".into(),
-                    tag_value: Some(StringMatch {
-                      string_match_type: Some(String_match_type::Exact("exact_value".into())),
-                      ..Default::default()
-                    })
-                    .into(),
-                    ..Default::default()
-                  })),
-                  ..Default::default()
-                },
-                DropCondition {
-                  condition_type: Some(Condition_type::TagMatch(TagMatch {
-                    tag_name: "tag_regex".into(),
-                    tag_value: Some(StringMatch {
-                      string_match_type: Some(String_match_type::Exact("regex_value.*".into())),
-                      ..Default::default()
-                    })
-                    .into(),
-                    ..Default::default()
-                  })),
-                  ..Default::default()
-                },
+                make_exact_match("exact_name"),
+                make_regex_match("regex_name.*"),
+                make_tag_match("tag_present"),
+                make_tag_value_exact_match("tag_exact", "exact_value"),
+                make_tag_value_regex_match("tag_regex", "regex_value.*"),
               ],
               ..Default::default()
             },
             DropRule {
               name: "rule2".into(),
               mode: DropMode::TESTING.into(),
-              conditions: vec![DropCondition {
-                condition_type: Some(Condition_type::AndMatch(AndMatch {
-                  conditions: vec![
-                    DropCondition {
-                      condition_type: Some(Condition_type::MetricName(StringMatch {
-                        string_match_type: Some(String_match_type::Exact(
-                          "value_match_name".into(),
-                        )),
-                        ..Default::default()
-                      })),
+              conditions: vec![make_and_match(vec![
+                make_exact_match("value_match_name"),
+                DropCondition {
+                  condition_type: Some(Condition_type::ValueMatch(ValueMatch {
+                    value_match_type: Some(Value_match_type::SimpleValue(SimpleValueMatch {
+                      target: 100.0,
+                      operator: ValueMatchOperator::NOT_EQUAL.into(),
                       ..Default::default()
-                    },
-                    DropCondition {
-                      condition_type: Some(Condition_type::ValueMatch(ValueMatch {
-                        value_match_type: Some(Value_match_type::SimpleValue(SimpleValueMatch {
-                          target: 100.0,
-                          operator: ValueMatchOperator::NOT_EQUAL.into(),
-                          ..Default::default()
-                        })),
-                        ..Default::default()
-                      })),
-                      ..Default::default()
-                    },
-                  ],
+                    })),
+                    ..Default::default()
+                  })),
                   ..Default::default()
-                })),
-                ..Default::default()
-              }],
+                },
+              ])],
               ..Default::default()
             },
           ],
